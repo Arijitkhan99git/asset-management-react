1bcf6997ef23b5f1524b1aa9ae40d90f4815fb0e
Arijitkhan99git <arijitkhan52@gmail.com>
2025-08-10 22:49:40 +0530

    Rename the project name

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,21 @@ import Assignment from './Components/Assignment/Assignment.jsx'
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 
-
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Login />} />
+//       <Route path="/homepage" element={<Layout></Layout>} >
+//         <Route path='' element={<Dashboard/>}/>
+//         <Route path='/homepage/users' element={<Users></Users>}/>
+//         <Route path='/homepage/assets' element={<Assets/>}/>
+//         <Route path='/homepage/assignments' element={<Assignment/>}/>
+//       </Route>
+//     </Routes>
+//     </BrowserRouter>  
+//   </StrictMode>
+// )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
