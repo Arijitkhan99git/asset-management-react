00c9648674d1ef586f437f7f8f5807dbc9e3f13f
Arijitkhan99git <arijitkhan52@gmail.com>
2025-08-10 19:18:15 +0530

    Changed the App title

--- src/main.jsx
+++ src/main.jsx
@@ -13,22 +13,6 @@ import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//     <Routes>
-//       <Route path="/" element={<Login />} />
-//       <Route path="/homepage" element={<Layout></Layout>} >
-//         <Route path='' element={<Dashboard/>}/>
-//         <Route path='/homepage/users' element={<Users></Users>}/>
-//         <Route path='/homepage/assets' element={<Assets/>}/>
-//         <Route path='/homepage/assignments' element={<Assignment/>}/>
-//       </Route>
-//     </Routes>
-//     </BrowserRouter>  
-//   </StrictMode>
-// )
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
