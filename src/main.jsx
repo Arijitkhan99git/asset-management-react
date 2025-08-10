920f6625202ace412630a0e38b76b7ab56b722cb
Arijitkhan99git <arijitkhan52@gmail.com>
2025-08-10 22:53:02 +0530

    modify the main file, remove some unused path

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import { Route, Routes , BrowserRouter} from 'react-router-dom'
-import Login from './components/Login/Login.jsx'
-import Layout from './Layout.jsx'
-import Dashboard from './Components/Dashboard/Dashboard.jsx'
-import Users from './Components/Users/Users.jsx'
-import Assets from './Components/Assets/Assets.jsx'
-import Assignment from './Components/Assignment/Assignment.jsx'
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 
